066a4472494c492dc7300cfaa3feffac0c23c0ba
Tanisha06-Modi <tanishamodi2520@gmail.com>
2022-01-29 16:23:44 +0530

    added msg in Multiply.js

---
+++ Multiply.js
@@ -0,0 +1,9 @@
+
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+
+const product = num1 * num2;
+
+
+console.log(`The product of the nos ${num1} and ${num2} is ${product}`);
